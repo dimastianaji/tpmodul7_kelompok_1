ded9387f5900b251e20ec5da17c98a42aa0faaaf
Dimastian Aji Wibowo <dimastian.exe@gmail.com>
2025-04-03 16:17:41 +0700

    menambahkan class KuliahMahasiswa2311104058

--- tpmodul7_kelompok_1/tpmodul7_kelompok_1/Program.cs
+++ tpmodul7_kelompok_1/tpmodul7_kelompok_1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("=== Membaca Data Mahasiswa ===");
         DataMahasiswa2311104058.ReadJSON();
+
+        string filePath = @"tp7_2_2311104058.json";  
+
+        KuliahMahasiswa2311104058 kuliah = new KuliahMahasiswa2311104058();
+
+        kuliah.ReadJSON(filePath);
     }
 }
